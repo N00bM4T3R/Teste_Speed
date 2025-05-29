cfbcb72ba766489cd97d17bd88196cf27ffc58b4
N00bM4T3R <sabinolbn1810@gmail.com>
2025-05-29 18:34:33 -0300

    binary speed

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -1,30 +1,72 @@
 package aplication;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
+
+
 
 public class program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Integer[] vect = new Integer[100000000];
-		Boolean stop = false;
-		
+	
+		int n = 99999999;
 		for (int i = 0; i < 100000000; i++) {
             vect[i] = i ;
 		}
-		
-		int n = 99999999;
-		for (int i1 = 0; stop == false; i1++ ) {
-			if (n == vect[i1]) {
-				System.out.println("numero encontrado: " + vect[i1]);
-				stop = true;
-				
-			}
-			// 1 second e 480 milesecons
-		}
+	
+    
+		binary(vect, n);
+		System.out.println(linear(vect, n));
+	
 		
 		
 
+	}
+	public static int linear( Integer[] vect, Integer number) {
+		
+	   for (int i =0; i< vect.length ; i++) {
+		   if(vect[i].doubleValue() == number) {
+			   return vect[i];
+			   
+		   }
+		   
+	   }
+	   return 0;
+	   
+		
+	}
+	public static  int binary(Integer[] vect, Integer item) {
+		int low = 0;
+		int high = vect.length;
+		while (low <= high ) {
+			int middle = (low + high) /2;
+			int kick = vect[middle];
+			
+			if (kick == item) {
+				 System.out.println("posição encontrada: " + kick); 
+				return kick;
+				
+				
+			}
+			else if ( kick > item) {
+				high = middle -1;
+				
+			}
+			else {
+				low =middle +1;
+			}
+		
+			
+			
+		}
+		return 0;
+		
+		
+		
+		
+		
 	}
 	
 
